b81b16dcd80d79dc6f86a43f62d837b4c776cc38
Wibby Bernaldi <wibbybernaldi@gmail.com>
2015-09-30 15:22:27 +0700

    Tugas Warna
    PAk ini Tugas Saya

---
+++ Tugas Warna/WindowsApplication1/WindowsApplication1/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If MyBase.BackColor = Color.CadetBlue Then
+            MyBase.BackColor = Color.FloralWhite
+        Else : MyBase.BackColor = Color.CadetBlue
+        End If
+    End Sub
+End Class
